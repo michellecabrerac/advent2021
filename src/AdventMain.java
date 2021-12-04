f6fc02dd2ab894a4719a7739888180f6559dbdf4
michelle <michellecabrerac@gmail.com>
2021-12-04 23:43:11 +0100

    Advent2021 - Answer 1 part 1

--- src/AdventMain.java
+++ src/AdventMain.java
@@ -1,5 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class AdventMain {
     public static void main(String [] args){
-        System.out.println("Ueee!");
+        AdventMain advent = new AdventMain();
+        // DAY 1
+        System.out.println("Answer day 1 : " + advent.getAnswwerday1());
+        // DAY 1 part 2
+        System.out.println("Answer day 1 part 2: "+ advent.getAnswwerday1Part2());
+
+    }
+    // TODO - Improve!!
+    private String getAnswwerday1(){
+        //
+        int count = 0;
+        Integer currentNumber = null;
+        Integer lastNumber = null;
+        try {
+            File myObj = new File("dia1.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                try{
+                    currentNumber = Integer.valueOf(line);
+                    if(lastNumber != null && currentNumber > lastNumber){
+                        count++;
+                    }
+                    lastNumber = currentNumber;
+                }
+                catch (NumberFormatException ex){
+                    ex.printStackTrace();
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return "" + count;
+    }
+    private String getAnswwerday1Part2(){
+        return "";
     }
 }
